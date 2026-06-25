470ee28263652a34cc62dc0458891431e19916d5
Christian Watts <wattschristian21@gmail.com>
2026-06-25 00:31:27 -0500

    Add documentation for Arithmetic Logic Unit (ALU)
    Added a comment block describing the ALU functionality and author information.

---
+++ rtl/alu.sv
@@ -0,0 +1,11 @@
+/**
+ * Arithmetic Logic Unit (ALU)
+ * 
+ * Supports basic RISC-V operations:
+ * - Arithmetic: ADD, SUB, SLT
+ * - Logical: AND, OR, XOR
+ * - Shifts: SLL, SRL, SRA
+ * 
+ * @author Christian Watts
+ * @version 1.0
+ */
